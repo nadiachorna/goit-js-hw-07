1e02d9d819d671680d23eff04138d8ea8ddae48e
nadiachorna <96414740+nadiachorna@users.noreply.github.com>
2022-06-05 16:06:30 +0300

    hw-07.js

---
+++ js/02-lightbox.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+function onCreateGalleryItem(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    }).join("");
+}
+
+const galleryItem = onCreateGalleryItem(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryItem);
+
+gallery.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+    event.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250})
+
+}
+
